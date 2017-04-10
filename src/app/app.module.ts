2ed4619bde37761502068207a05120fb8fa82fc8
laker007 <838205692@qq.com>
2017-04-10 19:20:09 +0800

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { MineModule } from '../pages/mine/mine.module';
 import { QueryModule } from '../pages/query/query.module';
+import { Logon } from '../pages/logon/logon';
+
+import { LogonStatusService } from '../providers/logon-status-service';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,27 +20,30 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    Logon
   ],
   imports: [
     BrowserModule,
+    IonicStorageModule.forRoot(),
     IonicModule.forRoot(MyApp),
+    QueryModule,
     MineModule,
-    QueryModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    Logon
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Storage,
+    LogonStatusService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
