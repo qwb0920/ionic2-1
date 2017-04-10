f275c6a654cc63ac0c940b380e424a42b3b9f512
laker007 <838205692@qq.com>
2017-04-10 10:02:19 +0800

    add mine and query module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { MineModule } from '../pages/mine/mine.module';
+import { QueryModule } from '../pages/query/query.module';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -21,7 +23,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    MineModule,
+    QueryModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
