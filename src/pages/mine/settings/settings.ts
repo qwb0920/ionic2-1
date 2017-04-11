58d18da2758674c9d68423500eec485c80caeec2
laker007 <838205692@qq.com>
2017-04-11 21:11:57 +0800

    settings page add

---
+++ src/pages/mine/settings/settings.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the Query page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@Component({
+  selector: 'page-settings',
+  templateUrl: 'settings.html',
+})
+export class Settings {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Settings');
+  }
+
+}
