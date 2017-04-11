859e83255c393af02cedd0f2833803e8328ef08e
laker007 <838205692@qq.com>
2017-04-11 15:26:06 +0800

    file create

---
+++ src/providers/config.ts
@@ -0,0 +1,7 @@
+export const ServiceRootPath = "http://192.168.62.39:8180/ALS7M/JSONService?method=";
+export const ServiceRealRootPath = "http://192.168.62.39:8180/ALS7M";
+
+export const UUID = "00:9a:cd:50:dd:42";
+
+// TODO:不能写死，通过platform方式去获取
+export const DeviceType = "ios";
