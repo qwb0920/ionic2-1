ba03844b240792e6fb5370437ccf1c4f9e67f1e9
laker007 <838205692@qq.com>
2017-04-11 15:25:44 +0800

    add form and logon rules

--- src/pages/logon/logon.ts
+++ src/pages/logon/logon.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {FormBuilder, Validators} from '@angular/forms';
 
 import { MyApp } from '../../app/app.component';
 
 import { LogonStatusService } from '../../providers/logon-status-service';
+import { HttpService } from '../../providers/http-service';
 
 /**
  * Generated class for the Logon page.
@@ -16,17 +18,33 @@ import { LogonStatusService } from '../../providers/logon-status-service';
   templateUrl: 'logon.html',
 })
 export class Logon {
+  public logonForm;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public logonStatusService: LogonStatusService) {
+  constructor(public navCtrl: NavController,  public logonStatusService: LogonStatusService, public formBuilder: FormBuilder, public httpService: HttpService) {
+    this.logonForm = this.formBuilder.group({
+      UserId: ["test11", [Validators.required, Validators.minLength(4)]],// 第一个参数是默认值
+      UserPwd: ["000000als", [Validators.required, Validators.minLength(4)]]
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Logon');
   }
 
-  Logon(){
-    this.logonStatusService.Logon();
-    this.navCtrl.push(MyApp)
+  Logon(user){
+    console.log(user);
+    // this.navCtrl.push(MyApp);
+    
+    this.httpService.post('logon',user).subscribe((data) => {
+      if(data.Result === 'Y'){
+          // 登录成功
+          console.log(data.user);
+          this.logonStatusService.Logon();
+          this.navCtrl.push(MyApp,null,{animation:'ios-transition'});
+      }else{
+        console.log('登录失败');
+      }
+    })
   }
 
 }
