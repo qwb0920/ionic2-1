d4813be81e685bd252db4df72ed88d61f4444e04
laker007 <838205692@qq.com>
2017-04-11 19:16:32 +0800

    logon service update

--- src/pages/logon/logon.ts
+++ src/pages/logon/logon.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, AlertController, LoadingController } from 'ionic-angular';
 import {FormBuilder, Validators} from '@angular/forms';
 
 import { MyApp } from '../../app/app.component';
@@ -20,30 +20,50 @@ import { HttpService } from '../../providers/http-service';
 export class Logon {
   public logonForm;
 
-  constructor(public navCtrl: NavController,  public logonStatusService: LogonStatusService, public formBuilder: FormBuilder, public httpService: HttpService) {
+  constructor(public navCtrl: NavController,  public logonStatusService: LogonStatusService, public formBuilder: FormBuilder, 
+  public httpService: HttpService, public alertCtrl: AlertController, public loadingCtrl: LoadingController) {
     this.logonForm = this.formBuilder.group({
       UserId: ["test11", [Validators.required, Validators.minLength(4)]],// 第一个参数是默认值
       UserPwd: ["000000als", [Validators.required, Validators.minLength(4)]]
     });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad Logon');
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: '网络请求超时',
+      subTitle: '请再次检查你的网络配置',
+      buttons: ['确定']
+    });
+    alert.present();
+  }
+
+  showLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 4500
+    });
+    loader.present();
   }
 
   Logon(user){
-    console.log(user);
     // this.navCtrl.push(MyApp);
-    
-    this.httpService.post('logon',user).subscribe((data) => {
+    this.showLoading();
+    this.httpService.post('logon',user).subscribe(
+      data => {
       if(data.Result === 'Y'){
           // 登录成功
           console.log(data.user);
-          this.logonStatusService.Logon();
-          this.navCtrl.push(MyApp,null,{animation:'ios-transition'});
+          this.logonStatusService.LogonSucc();
+          this.navCtrl.push(MyApp);
+          
       }else{
         console.log('登录失败');
+        this.logonStatusService.LogonFail();
       }
+    },
+    error => {
+      this.showAlert();
+      console.log(error);
     })
   }
 
