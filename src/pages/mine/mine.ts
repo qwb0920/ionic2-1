e98f1ad8966d80e7f0cba6ff617e77b5c35288e2
laker007 <838205692@qq.com>
2017-04-12 20:24:57 +0800

    page update

--- src/pages/mine/mine.ts
+++ src/pages/mine/mine.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { MyApp } from '../../app/app.component';
+import { WorkLogs } from './worklogs/worklogs';
 import { Settings } from './settings/settings'; 
 
 import { LogonStatusService } from '../../providers/logon-status-service'
@@ -17,8 +18,23 @@ import { LogonStatusService } from '../../providers/logon-status-service'
   templateUrl: 'mine.html',
 })
 export class Mine {
+  public user ={
+    UserID:'',
+    OrgName:'',
+  };
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public logonStatusService: LogonStatusService) {
+      
+  }
+
+  ionViewDidLoad() {
+    this.logonStatusService.getUser().then((val) => {
+            this.user = val;
+          });
+  }
+
+  WorkLogs(){
+    this.navCtrl.push(WorkLogs);
   }
 
   Settings(){
