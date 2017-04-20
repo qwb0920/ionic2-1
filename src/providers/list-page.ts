7561807b6ee3adf73ca3e95ea3c24ee7a8330c4d
laker007 <838205692@qq.com>
2017-04-20 10:48:18 +0800

    list page update

--- src/providers/list-page.ts
+++ src/providers/list-page.ts
@@ -1,25 +1,34 @@
 import {OnInit} from '@angular/core';
-import {LogonStatusService} from './';
 
 export class ListPage implements OnInit {
 
-  public pageNo=1;
+  public pageNo = 1;
   public infiniteScroll;
+  public noData : boolean;
+  public items = [];
 
   ngOnInit() {
+    this.noData = false;
     console.log("ngOnInit")
   }
 
+  /**
+ * 下拉刷新页面
+ * @param event
+ */
   doRefresh(event) {
     this.pageNo = 1;
-    this.loadList(event,this.pageNo++);
+    this.loadList(event, this.pageNo++);
 
-    if(this.infiniteScroll){
-      this.infiniteScroll.enable(true);
+    // 在加载到最后一页，上拉加载的功能会被禁用。在刷新页面时，将上拉加载功能重新启用
+    if (this.infiniteScroll) {
+      this
+        .infiniteScroll
+        .enable(true);
     }
   }
 
-  loadList(event, pageNo?) {}
+  loadList(event, pageNo) {}
 
   doInfinite(infiniteScroll) {
     this.infiniteScroll = infiniteScroll
@@ -32,13 +41,30 @@ export class ListPage implements OnInit {
 
   }
 
-  complete(event,currentArrayLength,currentPageNo,pageSize,totalCount){
-    
-    if(event){
+  handleData(event, data, pageNo, pageSize) {
+
+    if (event) {
+      // 结束本次页面操作，包括上拉加载和下拉刷新
       event.complete();
+    } else {
+      // 没有event传入，此处为进入页面第一次加载listpage
+      this.pageNo++;
     }
-    console.log(event,currentArrayLength,currentPageNo,pageSize,totalCount);
-    if(currentArrayLength < pageSize || currentPageNo*pageSize == totalCount){
+
+    if (pageNo == 1) {
+      this.items = [];
+    }
+    for (let i = 0; i < data.array.length; i++) {
+      this
+        .items
+        .push(data.array[i]);
+    }
+    console.log(event, data, data.array.length, pageNo, pageSize);
+    if (data.totalCount == 0) {
+      this.noData = true;
+      return
+    }
+    if (data.array.length < pageSize || pageNo * pageSize == data.totalCount) {
       // 到了最后一页
       event.enable(false);
     }
